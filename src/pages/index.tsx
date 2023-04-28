dbd59afddde23b78b590488b96c262cda80fcc94
S.Zelensky <zelenstein@gmail.com>
2023-04-28 21:18:33 +0300

    Task-5: getStaticProps axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 import { Button, H, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home() {
+const Home: FC<HomeProps> = ({ menu }) => {
   const [rating, setRating] = useState<number>(3);
 
   return (
@@ -35,20 +38,65 @@ function Home() {
         маркетологами. Выпускники факультета могут успешно конкурировать с
         веб-дизайнерами уровня middle.
       </P>
-      <Tag size='m' color='ghost'>10 000 $</Tag>
-      <Tag size='m' color='red'>10 000 $</Tag>
-      <Tag size='m' color='green'>10 000 $</Tag>
-      <Tag size='m' color='gray'>10 000 $</Tag>
-      <Tag size='m' color='primary'>10 000 $</Tag>
+      <Tag size='m' color='ghost'>
+        10 000 $
+      </Tag>
+      <Tag size='m' color='red'>
+        10 000 $
+      </Tag>
+      <Tag size='m' color='green'>
+        10 000 $
+      </Tag>
+      <Tag size='m' color='gray'>
+        10 000 $
+      </Tag>
+      <Tag size='m' color='primary'>
+        10 000 $
+      </Tag>
       <br />
-      <Tag size='sm' color='ghost'>10 000 $</Tag>
-      <Tag size='sm' color='red'>10 000 $</Tag>
-      <Tag size='sm' color='green'>10 000 $</Tag>
-      <Tag size='sm' color='gray'>10 000 $</Tag>
-      <Tag size='sm' color='primary'>10 000 $</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
+      <Tag size='sm' color='ghost'>
+        10 000 $
+      </Tag>
+      <Tag size='sm' color='red'>
+        10 000 $
+      </Tag>
+      <Tag size='sm' color='green'>
+        10 000 $
+      </Tag>
+      <Tag size='sm' color='gray'>
+        10 000 $
+      </Tag>
+      <Tag size='sm' color='primary'>
+        10 000 $
+      </Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+      {menu.map((item) => (
+        <div key={item._id.secondCategory}>{item._id.secondCategory}</div>
+      ))}
     </>
   );
-}
+};
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    }
+  );
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
